7e61c97d81b191d6f3936bda7bc4204360f38ded
Asim14878111 <asim14875111@gmail.com>
2025-04-19 21:50:14 +0500

    Responsive,  desktop version completed and added 10mins timer

--- script.js
+++ script.js
@@ -38,6 +38,26 @@ intervalid = setInterval(updateTimer,1000)
 
 function savedata(){
     const inpvalue = document.getElementById('email-inp').value
-    const inp2value = document.getElementById('first-name-inp').value
+   const countries = document.getElementById("country").value
+   const fname = document.getElementById("first-name-inp").value
+   const lname = document.getElementById("last-name-inp").value
+   const address = document.getElementById("address").value
+   const city = document.getElementById("city").value
+   const postalcode = document.getElementById("postalcode").value
+   const cardnum = document.getElementById("card-num").value
+   const cardname = document.getElementById("card-name").value
+   const mmyy = document.getElementById("mm-yy").value
+   const cvv = document.getElementById("cvv").value
     localStorage.setItem("Email" , inpvalue)
+    localStorage.setItem("Country",countries)
+    localStorage.setItem("First name",fname)
+    localStorage.setItem("Last name",lname)
+    localStorage.setItem(" Address",address)
+    localStorage.setItem("City",city)
+    localStorage.setItem("Postal code",postalcode)
+    localStorage.setItem("Card Number",cardnum)
+    localStorage.setItem("Card Name",cardname)
+    localStorage.setItem("Month, year",mmyy) 
+    localStorage.setItem("Cvv",cvv) 
+
 }
